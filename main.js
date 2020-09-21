3af375dc1304e689833b5c684b934738e5d8730a
jose miguel <jmtordoya@gmail.com>
2020-09-21 19:43:05 -0400

    actualizacion

---
+++ main.js
@@ -0,0 +1,167 @@
+document.getElementById("n1").addEventListener("click", n1);
+document.getElementById("n5").addEventListener("click", n5);
+document.getElementById("n7").addEventListener("click", n7);
+document.getElementById("n14").addEventListener("click", n14);
+document.getElementById("n19").addEventListener("click", n19);
+document.getElementById("n23").addEventListener("click", n23);
+document.getElementById("n28").addEventListener("click", n28);
+document.getElementById("n35").addEventListener("click", n35);
+document.getElementById("n37").addEventListener("click", n37);
+document.getElementById("n40").addEventListener("click", n40);
+document.getElementById("n43").addEventListener("click", n43);
+document.getElementById("n47").addEventListener("click", n47);
+document.getElementById("n50").addEventListener("click", n50);
+document.getElementById("n52").addEventListener("click", n52);
+document.getElementById("n55").addEventListener("click", n55);
+document.getElementById("n57").addEventListener("click", n57);
+document.getElementById("n59").addEventListener("click", n59);
+document.getElementById("n60").addEventListener("click", n60);
+document.getElementById("n67").addEventListener("click", n67);
+document.getElementById("n70").addEventListener("click", n70);
+document.getElementById("n73").addEventListener("click", n73);
+document.getElementById("n77").addEventListener("click", n77);
+document.getElementById("n78").addEventListener("click", n78);
+document.getElementById("n80").addEventListener("click", n80);
+
+function n1(){
+    let zombies = document.getElementById("n1").innerHTML + "zombies murieron";
+    document.getElementById("zombies").innerHTML =  zombies;
+    document.getElementById("n1").innerHTML =  "X";
+}
+
+function n5(){
+    let zombies = document.getElementById("n5").innerHTML + "zombies murieron";
+    document.getElementById("zombies").innerHTML =  zombies;
+    document.getElementById("n5").innerHTML =  "X";
+}
+
+function n7(){
+    let zombies = document.getElementById("n7").innerHTML + "zombies murieron";
+    document.getElementById("zombies").innerHTML =  zombies;
+    document.getElementById("n7").innerHTML =  "X";
+}
+
+function n14(){
+    let zombies = document.getElementById("n14").innerHTML + "zombies murieron";
+    document.getElementById("zombies").innerHTML =  zombies;
+    document.getElementById("n14").innerHTML =  "X";
+}
+
+function n19(){
+    let zombies = document.getElementById("n19").innerHTML + "zombies murieron";
+    document.getElementById("zombies").innerHTML =  zombies;
+    document.getElementById("n19").innerHTML =  "X";
+}
+
+function n23(){
+    let zombies = document.getElementById("n23").innerHTML + "zombies murieron";
+    document.getElementById("zombies").innerHTML =  zombies;
+    document.getElementById("n23").innerHTML =  "X";
+}
+
+function n28(){
+    let zombies = document.getElementById("n28").innerHTML + "zombies murieron";
+    document.getElementById("zombies").innerHTML =  zombies;
+    document.getElementById("n28").innerHTML =  "X";
+}
+
+function n35(){
+    let zombies = document.getElementById("n35").innerHTML + "zombies murieron";
+    document.getElementById("zombies").innerHTML =  zombies;
+    document.getElementById("n35").innerHTML =  "X";
+}
+
+function n37(){
+    let zombies = document.getElementById("n37").innerHTML + "zombies murieron";
+    document.getElementById("zombies").innerHTML =  zombies;
+    document.getElementById("n37").innerHTML =  "X";
+}
+
+function n40(){
+    let zombies = document.getElementById("n40").innerHTML + "zombies murieron";
+    document.getElementById("zombies").innerHTML =  zombies;
+    document.getElementById("n40").innerHTML =  "X";
+}
+
+function n43(){
+    let zombies = document.getElementById("n43").innerHTML + "zombies murieron";
+    document.getElementById("zombies").innerHTML =  zombies;
+    document.getElementById("n43").innerHTML =  "X";
+}
+
+function n47(){
+    let zombies = document.getElementById("n47").innerHTML + "zombies murieron";
+    document.getElementById("zombies").innerHTML =  zombies;
+    document.getElementById("n47").innerHTML =  "X";
+}
+
+function n50(){
+    let zombies = document.getElementById("n50").innerHTML + "zombies murieron";
+    document.getElementById("zombies").innerHTML =  zombies;
+    document.getElementById("n50").innerHTML =  "X";
+}
+function n52(){
+    let zombies = document.getElementById("n52").innerHTML + "zombies murieron";
+    document.getElementById("zombies").innerHTML =  zombies;
+    document.getElementById("n52").innerHTML =  "X";
+}
+
+function n55(){
+    let zombies = document.getElementById("n55").innerHTML + "zombies murieron";
+    document.getElementById("zombies").innerHTML =  zombies;
+    document.getElementById("n55").innerHTML =  "X";
+}
+
+function n57(){
+    let zombies = document.getElementById("n57").innerHTML + "zombies murieron";
+    document.getElementById("zombies").innerHTML =  zombies;
+    document.getElementById("n57").innerHTML =  "X";
+}
+
+function n59(){
+    let zombies = document.getElementById("n59").innerHTML + "zombies murieron";
+    document.getElementById("zombies").innerHTML =  zombies;
+    document.getElementById("n59").innerHTML =  "X";
+}
+
+function n60(){
+    let zombies = document.getElementById("n60").innerHTML + "zombies murieron";
+    document.getElementById("zombies").innerHTML =  zombies;
+    document.getElementById("n60").innerHTML =  "X";
+}
+
+function n67(){
+    let zombies = document.getElementById("n67").innerHTML + "zombies murieron";
+    document.getElementById("zombies").innerHTML =  zombies;
+    document.getElementById("n67").innerHTML =  "X";
+}
+
+function n70(){
+    let zombies = document.getElementById("n70").innerHTML + "zombies murieron";
+    document.getElementById("zombies").innerHTML =  zombies;
+    document.getElementById("n70").innerHTML =  "X";
+}
+
+function n73(){
+    let zombies = document.getElementById("n73").innerHTML + "zombies murieron";
+    document.getElementById("zombies").innerHTML =  zombies;
+    document.getElementById("n73").innerHTML =  "X";
+}
+
+function n77(){
+    let zombies = document.getElementById("n77").innerHTML + "zombies murieron";
+    document.getElementById("zombies").innerHTML =  zombies;
+    document.getElementById("n77").innerHTML =  "X";
+}
+
+function n78(){
+    let zombies = document.getElementById("n78").innerHTML + "zombies murieron";
+    document.getElementById("zombies").innerHTML =  zombies;
+    document.getElementById("n78").innerHTML =  "X";
+}
+
+function n80(){
+    let zombies = document.getElementById("n80").innerHTML + "zombies murieron";
+    document.getElementById("zombies").innerHTML =  zombies;
+    document.getElementById("n80").innerHTML =  "X";
+}
